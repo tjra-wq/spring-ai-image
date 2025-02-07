427fe3b0cf52fc25a664b43ecfc7112e88070f52
TJR <tjra077@gmail.com>
2025-02-07 12:29:24 -0500

    Initial commit

---
+++ src/test/java/com/tjr/springaiimage/SpringAiImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjr.springaiimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
